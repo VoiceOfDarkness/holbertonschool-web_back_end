9f5167d9ce76aabee13f36e576b3ac3a8247d519
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-30 14:07:01 +0400

    7th task

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Sum of a mixed list"""
+from typing import List, Any
+
+
+def sum_mixed_list(mxd_lst: List[Any]) -> float:
+    """Sum of a mixed list"""
+
+    return sum(mxd_lst)

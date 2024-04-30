cad6aebbe1ec3defd27ba57bf941126d20295918
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-30 14:08:01 +0400

    changed to union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Sum of a mixed list"""
-from typing import List, Any
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Any]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Sum of a mixed list"""
 
     return sum(mxd_lst)

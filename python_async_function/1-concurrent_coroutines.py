b975663f509b67d697d159a97412fcfd42a2009b
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-03 18:22:57 +0400

    hot fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,4 +11,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Wait for a random delay between 0 and `max_delay` seconds
     """
     delays = [await wait_random(max_delay) for _ in range(n)]
-    return delays
+    return sorted(delays)

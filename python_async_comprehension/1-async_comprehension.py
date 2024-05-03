d2097e14e1b105e5c3f6b94dfa47917df2cb623c
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-03 19:35:38 +0400

    2nd task

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """Collect 10 random numbers using an async comprehensing"""
+
+    return [i async for i in async_generator()]

6d1544bd18419b8bd5dd9caca319c9cf4cfff8a6
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-03 19:50:34 +0400

    3rd task

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Measure the runtime
+"""
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the runtime"""
+
+    start = asyncio.get_event_loop().time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end = asyncio.get_event_loop().time()
+    return end - start

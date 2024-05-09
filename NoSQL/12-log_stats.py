785b9d3d81d13f8df635bf88429f25944520e7fa
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-05-09 12:57:16 +0400

    deleted function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,19 +3,16 @@
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection):
+if __name__ == "__main__":
     """Log stats"""
 
-    print('{} logs'.format(mongo_collection.count_documents()))
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    print('{} logs'.format(logs_collection.count_documents()))
     print('Methods:')
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = mongo_collection.count_documents({"method": method})
+        count = logs_collection.count_documents({"method": method})
         print('\tmethod {}: {}'.format(method, count))
-    print('{} status check'.format(mongo_collection.count_documents({})))
-
-
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    log_stats(logs_collection)
+    print('{} status check'.format(logs_collection.count_documents({})))

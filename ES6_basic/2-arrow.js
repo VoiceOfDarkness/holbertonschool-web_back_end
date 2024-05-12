439b46ef4c26882da7a6675d1ad634e1ff805bfe
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-05-12 12:52:45 +0400

    hot fix

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+    this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
-  const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
-  };
+    const self = this;
+    this.addNeighborhood = (newNeighborhood) => {
+        this.sanFranciscoNeighborhoods.push(newNeighborhood);
+        return this.sanFranciscoNeighborhoods;
+    };
 }

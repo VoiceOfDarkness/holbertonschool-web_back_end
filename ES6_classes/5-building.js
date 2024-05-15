90381caacb38b00b72dc97dead437e4ec6c817f7
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-15 19:33:58 +0300

    6th task

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(
+      "Class extending Building must override evacuationWarningMessage"
+    );
+  }
+  
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}

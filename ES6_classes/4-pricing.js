6886a387f5fd5067588e9482ae6e68832629cb0e
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-15 19:32:02 +0300

    added setter and getter

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -4,16 +4,33 @@ import Currency from './3-currency';
 
 
 export default class Pricing {
-    constructor(price, currency) {
-        this.price = price;
-        this.currency = Currency;
+    constructor(amount, currency) {
+      this._amount = amount;
+      this._currency = currency;
     }
-
+  
     displayFullPrice() {
-        return `${this.price} - ${this.currency.displayFullCurrency()}`;
+      return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
+    }
+  
+    static convertPrice(amount, conversionRate) {
+      return amount * conversionRate;
+    }
+  
+    set amount(newAmount) {
+      this._amount = newAmount;
+    }
+  
+    set currency(newCurrency) {
+      if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+      this._currency = newCurrency;
     }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
+    get amount() {
+      return this._amount;
     }
-}
+  
+    get currency() {
+      return this._currency;
+    }
+  }

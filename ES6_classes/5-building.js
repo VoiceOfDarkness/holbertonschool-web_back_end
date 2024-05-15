5bd484b3740b9d52c35bc21af5a635478efbb2c5
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-15 19:35:49 +0300

    hot fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,20 +2,15 @@
 
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage)
+      throw Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+
     this._sqft = sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error(
-      "Class extending Building must override evacuationWarningMessage"
-    );
-  }
-  
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(newSqft) {
-    this._sqft = newSqft;
   }
 }

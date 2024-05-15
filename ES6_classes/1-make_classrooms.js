5422e931c1314f3e684f59535ccb7034f0322f68
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-15 16:30:11 +0300

    hot fix

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
 
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    return [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
+  const sizes = [19, 20, 34];
+  const classArray = [];
+
+  for (const size of sizes) {
+    classArray.push(new ClassRoom(size));
+  }
+  return classArray;
 }

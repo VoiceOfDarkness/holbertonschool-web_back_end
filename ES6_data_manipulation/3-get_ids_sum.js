c1d913a00ac3bb494de9bf69e8d3c282a9911ccd
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-25 19:53:46 +0300

    4th task

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+
+export default function getStudentIdsSum(array) {
+    if (!Array.isArray(array)) return [];
+    return array.reduce((sum, student) => sum + student.id, 0);
+}

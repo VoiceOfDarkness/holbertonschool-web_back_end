63afba82e41dc2b56d91417a2c7ef134c6a46ef7
VoiceOfDarkness <abil.samedov2016@yandex.ru>
2024-05-25 20:35:46 +0300

    hot fix

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) return "Cannot process";
+  if (!(map instanceof Map)) {
+    throw new Error("Cannot process");
+  }
   for (const [key, value] of map) {
     if (value === 1) map.set(key, 100);
   }
